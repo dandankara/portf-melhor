b05b3dd98f0c682b581c6fa676cc551d73b272cd
Daniel Cunha <65553049+dandankara@users.noreply.github.com>
2021-02-11 17:31:29 -0300

    Update Sobre.js

--- src/components/Sobre/Sobre.js
+++ src/components/Sobre/Sobre.js
@@ -14,8 +14,8 @@ class Sobre extends Component{
         Hoje, Daniel Cunha estudante e FrontEnd Developer, formado em técnico em informática
         atuou na área em 2018 pela Cooper Rubi juntamente com a Rede Jovem Aprendiz, era responsável pela manunteção
         de equipamentos em geral na empresa, cadastro de usuários na época da safra. Atualmente busca
-        o desenvolvimento pessoal e na área de Ti, usufruindo de linguagens como JavaScript e bibliotecas 
-        como React e React Native. Atualmente buscando uma oportunidade em desenvolvimento voltado para FrontEnd,
+        o desenvolvimento pessoal e na área de Ti, usufruindo de linguagens como JavaScript. 
+      Atualmente buscando uma oportunidade em desenvolvimento voltado para FrontEnd,
         sempre mantendo organização de seus projetos no Github e se aventurando nessa área de desenvolvimento.
       </About>
 
